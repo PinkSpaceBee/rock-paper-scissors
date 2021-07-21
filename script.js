1f1476f17693ee55790dbb25c69f69dedf6e7174
PinkSpaceBee <minaary@mailfence.com>
2021-07-21 11:54:22 +0300

    Add playRound() function

--- script.js
+++ script.js
@@ -25,4 +25,17 @@ function computerPlay() {
     return assignNumToShape();
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    computerSelection = computerPlay(); // returns string value
+    playerSelection = prompt('rock, paper os scissors?').toLocaleLowerCase();
+    
+    let combinations = ['rock','paper','scissors','rock','paper','scissors','rock'];
+
+    if (playerSelection === computerSelection) {
+        return `It's a tie!`;
+    } else if (combinations.join('').includes(playerSelection + computerSelection)) {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+}
