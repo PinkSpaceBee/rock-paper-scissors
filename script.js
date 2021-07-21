81affa75ee8987c507e18c4bb56dd2a1228be473
PinkSpaceBee <minaary@mailfence.com>
2021-07-21 15:48:05 +0300

    add game() function

--- script.js
+++ script.js
@@ -25,17 +25,50 @@ function computerPlay() {
     return assignNumToShape();
 }
 
-function playRound(playerSelection, computerSelection) {
-    computerSelection = computerPlay(); // returns string value
-    playerSelection = prompt('rock, paper os scissors?').toLocaleLowerCase();
-    
-    let combinations = ['rock','paper','scissors','rock','paper','scissors','rock'];
+function game() {
+    let score = 0;
+    let roundResult; 
+    let gameResult;
 
-    if (playerSelection === computerSelection) {
-        return `It's a tie!`;
-    } else if (combinations.join('').includes(playerSelection + computerSelection)) {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
-    } else {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+    function playRound(playerSelection, computerSelection) {
+        computerSelection = computerPlay(); // returns string value
+        playerSelection = prompt('rock, paper os scissors?').toLocaleLowerCase();
+        
+        let combinations = ['rock','paper','scissors','rock','paper','scissors','rock'];
+
+        if (playerSelection === computerSelection) {
+            roundResult = `It's a tie!`;
+        } else if (combinations.join('').includes(playerSelection + computerSelection)) {
+            roundResult = `You lose! ${computerSelection} beats ${playerSelection}`;
+        } else {
+            roundResult = `You win! ${playerSelection} beats ${computerSelection}`;
+        }
+
+        return roundResult;
     }
+
+    function countScore() {
+        for(let i = 0; i < 5; i++) {
+            playRound();
+            console.log(roundResult);
+    
+            if (roundResult.includes('win')) {
+                score++;
+            } else if (roundResult.includes('lose')) {
+                score--;
+            } else {
+                score;
+            }
+        }
+
+        return score;
+    }
+
+    countScore();
+    
+    gameResult = score < 0 ? 'You lost the game'
+        : score > 0 ? 'You won the game, congrats!'
+        : 'the game is a tie';
+
+    console.log(gameResult);
 }
