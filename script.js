3d44983807738e64eb3b6b86d441d2897d12672a
PinkSpaceBee <minaary@mailfence.com>
2021-08-06 15:56:21 +0300

    add div for displaying results

--- script.js
+++ script.js
@@ -1,110 +1,66 @@
-function computerPlay() {
-
-    function getRandomNum(min, max) {
-        let randomNum = min + Math.random() * (max + 1 - min);
-        return Math.floor(randomNum);
-    }
-
-    function playComputerRound() {
-
-        switch (getRandomNum(0, 2)) {
-            case 0: 
-            return 'rock';
-            break;
-
-            case 1: 
-            return 'paper';
-            break;
-
-            case 2: 
-            return 'scissors';
-            break;
-        }
-    }
-
-    return playComputerRound();
-}
-let playerSelection;
-let computerSelection;
-
-function selectAShape(btn) {
-    playerSelection = btn.textContent.toLowerCase();
-    return playerSelection;
-}
-function game() {
-    function playRound() {
-        playerSelection = selectAShape();
-        computerSelection = computerPlay(); // returns string value
-        
-        let combinations = ['rock','paper','scissors','rock','paper','scissors','rock'];
-
-        if (playerSelection === computerSelection) {
-            roundResult = `It's a tie!`;
-        } else if (combinations.join('').includes(playerSelection + computerSelection)) {
-            roundResult = `You lose! ${computerSelection} beats ${playerSelection}`;
-        } else {
-            roundResult = `You win! ${playerSelection} beats ${computerSelection}`;
-        }
-
-        return roundResult;
-        
-    }
-
-    
-   console.log(playRound());
-}
-
-/* My notes. Works correctly, just need to add the logic that determines
-* the winner and make it look pretty 
-const buttons = document.querySelectorAll('button');
-
-function computerPlay() {
-
-    function getRandomNum(min, max) {
-        let randomNum = min + Math.random() * (max + 1 - min);
-        return Math.floor(randomNum);
-    }
-
-    function playComputerRound() {
-
-        switch (getRandomNum(0, 2)) {
-            case 0: 
-            return 'rock';
-            break;
-
-            case 1: 
-            return 'paper';
-            break;
-
-            case 2: 
-            return 'scissors';
-            break;
-        }
-    }
-
-    return playComputerRound();
-}
-
 let playerSelection;
 let computerSelection = computerPlay();
-computerPlay();
 
+function computerPlay() {
 
-function selectAShape(btn) {
+    function getRandomNum(min, max) {
+        let randomNum = min + Math.random() * (max + 1 - min);
+        return Math.floor(randomNum);
+    }
+
+    function playComputerRound() {
+
+        switch (getRandomNum(0, 2)) {
+            case 0: 
+            return 'rock';
+            break;
+
+            case 1: 
+            return 'paper';
+            break;
+
+            case 2: 
+            return 'scissors';
+            break;
+        }
+    }
+
+    return playComputerRound();
+}
+
+function selectShape(btn) {
     playerSelection = btn.textContent.toLowerCase();
     return playerSelection;
 }
 
 function playRound() {
-    return playerSelection + ' ' + computerPlay();
+    /* I know this looks awful and a simple comparison of the playerSelecton and the computerSelection would do the trick (also, it would be much easier to read and understand), but I wanted to have some fun :D */
+
+    computerSelection = computerPlay();
+    let roundResult;
+
+    let combinations = ['rock','paper','scissors','rock','paper','scissors','rock'];
+
+    if (playerSelection === computerSelection) {
+        roundResult = `It's a tie!`;
+    } else if (combinations.join('').includes(playerSelection + computerSelection)) { 
+        roundResult = `You lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+        roundResult = `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+
+    return roundResult;
+
 }
 
+const buttons = document.querySelectorAll('button');
+
 
 buttons.forEach(button => button.addEventListener('click', () => {
-    selectAShape(button);
+    selectShape(button);
 }))
 
 buttons.forEach(button => button.addEventListener('click', () => {
-    console.log(playRound())
+    
 }))
-*/
+
